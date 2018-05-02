86feaf37f1addeb4b18064242af347ccf9c6e94a
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 09:06:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def turn(board)
   index = input_to_index(next_move)
   
   if valid_move?(board, index)
-    move(board, index, )
+    move(board, index, current_player)
 end
 
 def turn_count(board)
